63f0ef0d19ed5554d38d64c5e8799c46732d5b11
mahi <mahithegreat19@gmail.com>
2026-08-25 23:22:15 +0530

    push code

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 
 const generateToken = (user) => {
