20c419c153be47b3fe87c487a94e88c8c7b3789e
mahi <mahithegreat19@gmail.com>
2026-08-25 22:38:25 +0530

    update: register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
 
 function Register() {
+
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("Username:", formData.username);
+        console.log("Email:", formData.email);
+        console.log("Password:", formData.password);
+    }
 
     return (
         <>
             <h1>Register</h1>
-            <form>
-                <input type="text" placeholder="Username" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={formData.username}
+                    onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
                 <button type="submit">Register</button>
             </form>
         </>
