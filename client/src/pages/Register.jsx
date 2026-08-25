8f949b6ece66163f47330ba5a3d57477cdbeece2
mahi <mahithegreat19@gmail.com>
2026-08-25 22:06:09 +0530

    added: register prototype

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,18 @@
+
+function Register() {
+
+    return (
+        <>
+            <h1>Register</h1>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Register</button>
+            </form>
+        </>
+    );
+}
+
+export default Register;
+
