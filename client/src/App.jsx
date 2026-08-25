531e96e37d66a3a41956e420b35969c8d64e1919
mahi <mahithegreat19@gmail.com>
2026-08-25 22:37:12 +0530

    update: app module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import Login from './pages/Login'
 import Upload from './pages/Upload'
+import { Routes, Route } from 'react-router-dom'
+import Register from './pages/Register'
+
 
 function App() {
 
   return (
     <>
       <h2>Personal Finance Analyzer</h2>
-      <Login />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/upload' element={<Upload />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </>
   )
 }
