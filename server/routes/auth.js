1141439fd91bb22cd7b1e2e76969de84123955a6
mahi <mahithegreat19@gmail.com>
2026-08-25 23:23:19 +0530

    push code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,11 +2,11 @@ const { register, login, getUserProfile } = require('../controllers/auth');
 
 const express = require('express');
 const router = express.Router();
-const { authenticateToken } = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/profile', authenticateToken, getUserProfile);
+router.get('/profile', authMiddleware, getUserProfile);
 
 module.exports = router;
 
